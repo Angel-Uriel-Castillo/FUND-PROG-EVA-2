483c841d1dce400953cbd6e944f1b275e4a33f7a
Angel-Uriel-Castillo <143562793+Angel-Uriel-Castillo@users.noreply.github.com>
2023-10-24 17:49:05 -0600

    Add files via upload

---
+++ EVA2_9_SWITCH/src/main/java/com/mycompany/eva2_9_switch/EVA2_9_SWITCH.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_9_switch;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author uriel
+ */
+public class EVA2_9_SWITCH {
+
+    public static void main(String[] args) {
+        Scanner nono = new Scanner (System.in);
+        System.out.println("indica el numero del mez");
+        int mes = nono.nextInt();
+        
+        switch(mes){
+            case 1:
+                System.out.println("ENERO");
+                break;  
+            case 2:
+                System.out.println("FEBRERO");
+                break;  
+            case 3:
+                System.out.println("MARZO");
+                break;  
+            case 4:
+                System.out.println("ABRIL");
+                break;  
+            case 5:
+                System.out.println("MAYO");
+                break;  
+            case 6:
+                System.out.println("JUNIO");
+                break;  
+            case 7:
+                System.out.println("JULIO");
+                break;  
+            case 8:
+                System.out.println("AGOSO");
+                break;  
+            case 9:
+                System.out.println("SEPTIEMBRE");
+                break;  
+            case 10:
+                System.out.println("OCTUBRE");
+                break;  
+            case 11:
+                System.out.println("NOBIEMBRE");
+                break;  
+            case 12:
+                System.out.println("DICIEMBRE");
+                break; 
+            default:
+                System.out.println("el numero "+mes+ " no es un mes valido!!!");
+        }
+        
+    }
+}
